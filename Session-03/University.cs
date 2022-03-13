47d6d580f374e502ffaca99efa6154d40802b69d
GeorgeDoc <64958579+GeorgeDoc@users.noreply.github.com>
2022-03-13 10:34:46 +0200

    Accessed CourseForm listBox from Bussiness

--- Session-03/University.cs
+++ Session-03/University.cs
@@ -8,22 +8,29 @@ namespace Session_03
 {
     internal class Univeristy : Institute
     {
-        public Student[] Students { get; set; }
-        public Course[] Courses { get; set; }
-        public Grade[] Grades { get; set; }
-        public Schedule[] ScheduledCourses { get; set; }
+        //public Student[] Students { get; set; }
+        //public Course[] Courses { get; set; }
+        //public Grade[] Grades { get; set; }
+        //public Schedule[] ScheduledCourses { get; set; }
 
-        public Student[] GetStudents() { return Students; }
-        public Course[] GetCourses() { return Courses; }
-        public Grade[] GetGrades() { return Grades; }
+        public List<Student> Students { get; set; }
+        public List<Course> Courses { get; set; }
+        public List<Grade> Grades { get; set; }
+        public List<Schedule> ScheduledCourses { get; set; }
+        public List<Professor> Professors { get; set; }
+
+
+        public List<Student> GetStudents() { return Students; }
+        public List<Course> GetCourses() { return Courses; }
+        public List<Grade> GetGrades() { return Grades; }
         public void SetScedule(Guid CourseID, Guid ProfessorID, DateTime datetime) { }
 
 
         public Univeristy()
         {
-            Students = new Student[30];
-            Courses = new Course[20];
-            ScheduledCourses = new Schedule[60];
+            Students = new List<Student>(new Student[30]); //maybe?
+            Courses = new List<Course>(new Course[20]);
+            ScheduledCourses = new List<Schedule>(new Schedule[60]);
         }
     }
 }
