d140d844a49c52aa188e2ee3918986d154972e56
GeorgeDoc <64958579+GeorgeDoc@users.noreply.github.com>
2022-03-16 14:11:59 +0200

    .

---
+++ University2.0/Classes/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University2._0.Classes
+{
+    public class Course
+    {
+        public string ID { get; set; } = Guid.NewGuid().ToString();
+        public string Code { get; set; }
+        public string Subject { get; set; }
+    }
+}
