5301a19031e9e720ed107023f38f53bea55f17a2
GeorgeDoc <64958579+GeorgeDoc@users.noreply.github.com>
2022-03-16 14:15:13 +0200

    Created CoffeeShop Project

--- Coding-School-Team-Project/CoffeeSop.cs
+++ Coding-School-Team-Project/CoffeeSop.cs
@@ -1,8 +1,8 @@
 namespace Coding_School_Team_Project
 {
-    public partial class Form1 : Form
+    public partial class CoffeeSop : Form
     {
-        public Form1()
+        public CoffeeSop()
         {
             InitializeComponent();
         }
