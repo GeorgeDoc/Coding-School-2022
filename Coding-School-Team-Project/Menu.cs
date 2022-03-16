52aa767a98b3e153bbfaf3d9b1b3bb8e7e4d93fe
GeorgeDoc <64958579+GeorgeDoc@users.noreply.github.com>
2022-03-16 14:23:35 +0200

    Added some classes

---
+++ Coding-School-Team-Project/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coding_School_Team_Project
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
